1508e90bd184dbad1291009df704d6ed9fe7cc0a
Rahul H Sreedhar <rahul.h.1993@gmail.com>
2017-08-23 23:03:09 +0530

    sections

--- src/app/page/dashboard/dashboard.component.ts
+++ src/app/page/dashboard/dashboard.component.ts
@@ -9,9 +9,17 @@ import { AngularFireDatabase, FirebaseListObservable } from 'angularfire2/databa
 export class DashboardComponent implements OnInit {
 
   items: FirebaseListObservable<any[]>;
+  items1: FirebaseListObservable<any[]>;
 
   constructor(db: AngularFireDatabase) {
     this.items = db.list('/posts', {
+      query: {
+        limitToLast: 10,
+        orderByKey: true
+      }
+    });
+
+    this.items1 = db.list('/Challenges', {
       query: {
         limitToLast: 10,
         orderByKey: true
