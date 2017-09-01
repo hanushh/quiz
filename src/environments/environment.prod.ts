9b75b95095f333248fc0e9502a7dad94d6f5db06
Hanush H Nair <hnair@adobe.com>
2017-09-01 20:06:32 +0530

    update dependencies

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,11 +1,17 @@
 export const environment = {
     production: true,
     firebase: {
-         apiKey: "AIzaSyC88N5rWHfz2gLGiqOzjkdISfePxfOL-RY",
-    authDomain: "quiz-a2e13.firebaseapp.com",
-    databaseURL: "https://quiz-a2e13.firebaseio.com",
-    projectId: "quiz-a2e13",
-    storageBucket: "quiz-a2e13.appspot.com",
-    messagingSenderId: "279113692929"
+        apiKey: "AIzaSyC88N5rWHfz2gLGiqOzjkdISfePxfOL-RY",
+	    authDomain: "quiz-a2e13.firebaseapp.com",
+	    databaseURL: "https://quiz-a2e13.firebaseio.com",
+	    projectId: "quiz-a2e13",
+	    storageBucket: "quiz-a2e13.appspot.com",
+	    messagingSenderId: "279113692929",
+
+	    "rules": {    
+	        ".read": true,
+	        ".write": true
+	    }
+
     }
 };
